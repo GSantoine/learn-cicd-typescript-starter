dedb8ec1eeed1c371fd3c0a9e8afb4e7ffef49ed
agras <antoine.grameilda@gmail.com>
2026-09-23 20:03:36 +0200

    CI fail test

---
+++ src/tests/api-auth.test.ts
@@ -0,0 +1,40 @@
+import { IncomingHttpHeaders } from "http";
+import { getAPIKey } from "../api/auth.js";
+import { describe, expect, it } from "vitest";
+
+describe("getAPIKey tests", () => {
+  it("should return parsed key on valid authorization header", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "ApiKey TestKey",
+    };
+
+    const apiKey = getAPIKey(headers);
+    expect(apiKey).toBeTypeOf('string');
+    expect(apiKey?.length).toBeGreaterThan(0);
+  });
+
+  it("should return null on missing authorization header", () => {
+    const headers: IncomingHttpHeaders = {};
+
+    const apiKey = getAPIKey(headers);
+    expect(apiKey).toBeNull();
+  });
+
+  it("should return null on bad api key name", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "ApiKey TestKey",
+    };
+
+    const apiKey = getAPIKey(headers);
+    expect(apiKey).toBeNull();
+  });
+
+  it("should return null on missing api key value", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "ApiKey",
+    };
+
+    const apiKey = getAPIKey(headers);
+    expect(apiKey).toBeNull();
+  });
+});
